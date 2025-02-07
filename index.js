e5c2ad8feda25e6863d4610da567964f6455bcd8
Mahmat Bachar <ummahamat23@gmail.com>
2025-02-07 11:05:51 -0500

    completed calculator challenge Lab

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(){}
+function subtract(){}
+function multiply(){}
+function divide(){}
+function add(a, b){
+    return parseInt(a) + parseInt(b);
+}
+function subtract(a, b){
+    return parseInt(a) - parseInt(b);
+}
+function multiply(a, b){
+    return parseInt(a) * parseInt(b);
+}
+function divide(a, b){
+    return parseInt(a) / parseInt(b);
+}
+function increment(n){
+    n++;
+    return n;
+}
+function decrement(n){
+    n--;
+    return n;
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
